7132dff85e34935f0b777729b50d59b083163095
Ameer Deen <writeameer@gmmail.com>
2025-06-01 11:13:20 +0400

    updating logger

--- main.go
+++ main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
-
-	"github.com/hashicorp/go-hclog"
 	hplugin "github.com/hashicorp/go-plugin"
 
 	"github.com/katasec/dstream-ingester-mssql/mssql"
-	"github.com/katasec/dstream/pkg/logging"
 	"github.com/katasec/dstream/pkg/plugins/serve"
 )
 
 func main() {
-	// 🔒 Ensure stdlib log doesn't interfere with go-plugin handshake
-	log.SetOutput(os.Stderr)
-
-	// 🎯 Wrap your custom logger with hclog-compatible adapter
-	stdLogger := logging.GetLogger()
-	hclogAdapter := logging.NewHcLogAdapter(stdLogger)
-	hclogAdapter.SetLevel(hclog.Warn)
 
 	// 🚀 Start plugin with handshake and your logger
 	hplugin.Serve(&hplugin.ServeConfig{
@@ -31,16 +18,5 @@ func main() {
 			},
 		},
 		GRPCServer: hplugin.DefaultGRPCServer,
-		//Logger:     DiscardLogger(), // ✅ Now safely integrated
-	})
-}
-
-// DiscardLogger returns a logger that swallows everything.
-func DiscardLogger() hclog.Logger {
-	return hclog.New(&hclog.LoggerOptions{
-		Name:       "silent",
-		Level:      hclog.Off, // disables all logging
-		Output:     io.Discard,
-		JSONFormat: false,
 	})
 }
