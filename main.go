f4e7b68e0195b7bca454d40088affbd3279a4a0a
Ameer Deen <writeameer@gmail.com>
2025-06-08 14:58:01 +0400

    Testing plugins

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"os"
-
-	"github.com/hashicorp/go-hclog"
 	"github.com/katasec/dstream-ingester-mssql/mssql"
+	"github.com/katasec/dstream/pkg/logging"
 	"github.com/katasec/dstream/pkg/plugins/serve"
 )
 
 func main() {
-	// Create a plugin logger that writes to stderr
-	logLevel := hclog.Info
-	if level := os.Getenv("DSTREAM_LOG_LEVEL"); level != "" {
-		logLevel = hclog.LevelFromString(level)
-	}
-
-	// Check if JSON logging is enabled
-	jsonFormat := false
-	if jsonEnv := os.Getenv("DSTREAM_LOG_JSON"); jsonEnv == "true" || jsonEnv == "1" {
-		jsonFormat = true
-	}
-
-	logger := hclog.New(&hclog.LoggerOptions{
-		Name:       "mssql-plugin",
-		Level:      logLevel,
-		Output:     os.Stderr, // Important: use stderr, not stdout for plugin logs
-		JSONFormat: jsonFormat,
-	})
+	// Create a plugin logger using the SDK's SetupPluginLogger
+	// This creates a logger with no timestamps (host will add these)
+	logger := logging.SetupPluginLogger("mssql-plugin")
 
 	// Set the logger as a global for the mssql package to use
 	mssql.SetLogger(logger)
