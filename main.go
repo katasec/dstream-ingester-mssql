e5a2fdcecde362db750188d302c2a146006f86ac
Ameer Deen <writeameer@gmail.com>
2025-06-09 17:17:18 +0400

    Fixing logging

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"github.com/katasec/dstream-ingester-mssql/mssql"
-	"github.com/katasec/dstream/pkg/logging"
 	"github.com/katasec/dstream/pkg/plugins/serve"
+	"github.com/katasec/dstream/sdk/logging"
 )
 
 func main() {
 	// Create a plugin logger using the SDK's SetupPluginLogger
 	// This creates a logger with no timestamps (host will add these)
-	logger := logging.SetupPluginLogger("mssql-plugin")
-
+	//logger := logging.SetupPluginLogger("mssql-plugin")
+	logger := logging.SetupCleanLogger()
 	// Set the logger as a global for the mssql package to use
 	mssql.SetLogger(logger)
 
